f63500a19735c2408111da147ca300cf67b447f5
Marco <marco_x1230@hotmail.com>
2025-10-22 20:05:22 -0500

    Refactorizando el proyecto, quitando funciones innecesarias

--- api-gateway/src/main/java/com/cibertec/conf/SecurityConfig.java
+++ api-gateway/src/main/java/com/cibertec/conf/SecurityConfig.java
@@ -5,39 +5,43 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.setAllowedOrigins(List.of("http://localhost:4200"));
-	    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-	    config.setAllowedHeaders(List.of("*"));
-	    config.setExposedHeaders(List.of("Authorization")); // Optional
+	
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.setAllowedOrigins(List.of("http://localhost:4200"));
+	        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	        config.setAllowedHeaders(List.of("*"));
+	        config.setExposedHeaders(List.of("Authorization"));
 
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", config);
-	    return source;
-	}
-	
-	
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", config);
+	        return source;
+	    }
+
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
 	    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-	        .authorizeHttpRequests(authorize -> authorize
-	            .anyRequest().authenticated()
+	    .csrf(ServerHttpSecurity.CsrfSpec::disable)
+	    
+	    .authorizeExchange(authorize -> 
+	    authorize.anyExchange().authenticated()
 	        )
 	        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+	    //opcional mas confuso pero conciso .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt) lo mismo
 	    return http.build();
 	}
     
